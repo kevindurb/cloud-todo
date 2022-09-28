4511d6f4cc6cc3bb09378d910a68a02e6aebd90f
Kevin Durbin <kevindurb@gmail.com>
2022-09-28 10:08:36 -0600

    separate out spa and api into services dir

---
+++ services/spa/webpack.config.ts
@@ -0,0 +1,21 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
+import path from 'path';
+
+const config: Configuration = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
+
+export default config;
